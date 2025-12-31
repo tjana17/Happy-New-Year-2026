1a61a8a6315f803c2e3f06b823b5fcff713ac17e
Janarthanan Kannan <tjana17@gmail.com>
2025-12-31 12:25:32 +0530

    Initial Commit

---
+++ Happy New Year 2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Happy New Year 2026
+//
+//  Created by Janarthanan Kannan on 31/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Happy New Year 2026/Happy_New_Year_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_New_Year_2026App.swift
+//  Happy New Year 2026
+//
+//  Created by Janarthanan Kannan on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Happy_New_Year_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
